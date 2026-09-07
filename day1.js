4390bd92df7db5e4bc97d4979a69d2049bb226c7
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-09-07 16:11:40 -0700

    used const and created a greeitng instead of

--- day1.js
+++ day1.js
@@ -1,11 +1,16 @@
-let userName = "kel"
-let displayName = "Kelangit"
-let numOfList = 3
+const userName = "kel"
+const displayName = "Kelangit"
+const numOfList = 3
 
 let usrMsg = `Username: @${userName}`
 let disMsg = `Name: ${displayName}`
 let numMsg = `Lists:  ${numOfList}`
 
-console.log(usrMsg)
-console.log(disMsg)
-console.log(numMsg)
+// console.log(usrMsg)
+// console.log(disMsg)
+// console.log(numMsg)
+
+let greeting = `Welcome, ${displayName} (${userName})
+You currently have ${numOfList} lists`
+
+console.log(greeting)
