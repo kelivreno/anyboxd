613dfcbea477d08e0e163f80319347f32824c877
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-09-07 16:19:33 -0700

    added challenge2

---
+++ challenge2.js
@@ -0,0 +1,14 @@
+const listName =  "Countries I Want To Visit"
+const creatorName = "kel"
+const numOfItems = 50
+const completedItems = 12
+
+const listDisplay =  `
+=== ${listName} ===
+Created by @${creatorName}
+
+${completedItems} / ${numOfItems} completed
+
+`
+
+console.log(listDisplay)
