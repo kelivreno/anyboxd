751adbaccd52bdba53601a6fc49aae828139f2f5
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-09-07 16:26:25 -0700

    added multiplication and division

--- challenge2.js
+++ challenge2.js
@@ -2,13 +2,14 @@ const listName =  "Countries I Want To Visit"
 const creatorName = "kel"
 const numOfItems = 50
 const completedItems = 12
+const progress = Math.round(100*(completedItems/numOfItems))
 
 const listDisplay =  `
 === ${listName} ===
 Created by @${creatorName}
 
 ${completedItems} / ${numOfItems} completed
-
+Progress: ${progress}%
 `
 
 console.log(listDisplay)
