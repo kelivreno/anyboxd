2df8d725f59bc6c0cfaf832522782a0fad8c639a
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-09-07 17:11:18 -0700

    refactor: simplify conditionals and enhance item display with rating and meaning

--- challenge2.js
+++ challenge2.js
@@ -16,16 +16,29 @@ const itemType1 = "country"
 let symbol = ""
 let completed = true
 
-if (completed == true) {
+if (completed) {
     symbol =  "✓"
 } else {
     symbol =  "○"
 }
 
+let rating = 10
+let rateMeaning = ""
+
+if (rating <= 4) {
+    rateMeaning = "Not for me"
+} else if (rating <= 7) {
+    rateMeaning = "Good"
+} else {
+    rateMeaning = "Favorite"
+}
+
 const itemDisplay = `Current Item
 
 ${symbol} ${itemName1}
-Type: ${itemType1}`
+Type: ${itemType1}
+Rating ${rating}/10
+${rateMeaning}`
 
 console.log(listDisplay)
 console.log(itemDisplay)
