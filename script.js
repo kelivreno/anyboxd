c1d6444c1c113c989bc2abf02995a89c2f712e01
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-09-16 16:26:29 -0700

    feat: enhance add item form with submit functionality

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ const statusButtonHTML = document.querySelector(".status-button")
 const addButtonHTML = document.querySelector(".add-button")
 const addFormHTML = document.getElementById("add-form")
 const cancelButtonHTML = document.getElementById("cancel-button")
+const submitButtonHTML = document.getElementById("submit-item")
+let newItemName = ""
+let newItemType = ""
+
+const newItem = document.createElement("li")
 
 listNameHTML.textContent = listName
 progressHTML.textContent = progress
@@ -29,11 +34,19 @@ statusButtonHTML.addEventListener("click", function() {
 addButtonHTML.addEventListener("click", function() {
     addFormHTML.style.display = "block"
     addButtonHTML.style.display = "none"
+
 })
 
 cancelButtonHTML.addEventListener("click", function() {
     addFormHTML.style.display = "none"
     addButtonHTML.style.display = "block"
+})
+
+submitButtonHTML.addEventListener("click", function() {
+    newItemName = document.getElementById("item-name-input").value
+    newItemType = document.getElementById("item-type-input").value
+    console.log(newItemName)
+    console.log(newItemType)
 })
 
 
