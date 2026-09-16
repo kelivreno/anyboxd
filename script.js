d92b584672cf0c928aa52df45ec44482f5651cea
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-09-16 16:04:37 -0700

    feat: add item form with cancel functionality

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const progress = `${completedItems} / ${totalItems} completed - ${percentage}%`
 const listNameHTML = document.querySelector("#list-name")
 const progressHTML = document.querySelector("#progress")
 const statusButtonHTML = document.querySelector(".status-button")
+const addButtonHTML = document.querySelector(".add-button")
+const addFormHTML = document.getElementById("add-form")
+const cancelButtonHTML = document.getElementById("cancel-button")
 
 listNameHTML.textContent = listName
 progressHTML.textContent = progress
@@ -21,6 +24,16 @@ statusButtonHTML.addEventListener("click", function() {
     } else {
         statusButtonHTML.textContent = "✓"
     }
+})
+
+addButtonHTML.addEventListener("click", function() {
+    addFormHTML.style.display = "block"
+    addButtonHTML.style.display = "none"
+})
+
+cancelButtonHTML.addEventListener("click", function() {
+    addFormHTML.style.display = "none"
+    addButtonHTML.style.display = "block"
 })
 
 
